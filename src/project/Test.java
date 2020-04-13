02ca9c2d123c89d4544fe2002bc7276293611736
soddi <cvb7412@naver.com>
2020-04-13 17:11:32 +0900

    test

--- src/project/Test.java
+++ src/project/Test.java
@@ -5,6 +5,7 @@ public class Test {
 	public static void main(String[] args) {
 		System.out.println("팀프로젝트");
 		System.out.println("Merge Test1 영덕");
+		System.out.println("제가 만든 클래스는 안올라 간느건가요?");
 	}
 
 }
