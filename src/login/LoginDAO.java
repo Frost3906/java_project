f7d862bcbd496b9840ccace7295eb66446ed0676
joyui <secluionor@gmail.com>
2020-04-20 14:31:55 +0900

    dao

--- src/login/LoginDAO.java
+++ src/login/LoginDAO.java
@@ -1,6 +1,8 @@
 package login;
 
 import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 
 public class LoginDAO {
 
@@ -17,6 +19,14 @@ public class LoginDAO {
 		String url = "jdbc:oracle:thin:@localhost:1521:orcl";
 		String user = "javadb";
 		String password = "12345";
+		Connection con = null;
+	
+		try {
+			con = DriverManager.getConnection(url,user,password);
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		return con;
 	}
 	
 }
