2c7f27c12a1da898add84ad70698b52271c64403
joyui <secluionor@gmail.com>
2020-04-20 14:09:06 +0900

    login dao

--- src/login/Login.java
+++ src/login/Login.java
@@ -1,5 +1,70 @@
 package login;
 
-public class Login {
-//로그인
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import javax.swing.JLabel;
+import javax.swing.JTextField;
+import javax.swing.JButton;
+import java.awt.FlowLayout;
+import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class Login extends JFrame implements ActionListener{
+
+	private JPanel contentPane;
+	private final JLabel lb_id = new JLabel("ID");
+	private final JLabel lb_pw = new JLabel("PASSWORD");
+	private final JTextField txt_id = new JTextField();
+	private final JTextField txt_pw = new JTextField();
+	private final JButton btn_login = new JButton("로그인");
+	private final JButton btn_reg = new JButton("회원가입");
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					Login frame = new Login();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+
+	public Login() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 450, 300);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setContentPane(contentPane);
+		contentPane.setLayout(new GridLayout(0, 2, 0, 0));
+		contentPane.add(lb_id);
+		contentPane.add(txt_id);
+		txt_id.setColumns(10);
+		contentPane.add(lb_pw);
+		contentPane.add(txt_pw);
+		txt_pw.setColumns(10);
+		btn_login.addActionListener(this);
+		btn_reg.addActionListener(this);
+		contentPane.add(btn_reg);
+		contentPane.add(btn_login);
+	}
+
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
 }
--- src/login/LoginDAO.java
+++ src/login/LoginDAO.java
@@ -1,5 +1,22 @@
 package login;
 
+import java.sql.Connection;
+
 public class LoginDAO {
 
+	static {
+		try {
+			Class.forName("oracle.jdbc.OracleDriver");
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		}
+		
+	}
+	
+	public Connection getConnection() {
+		String url = "jdbc:oracle:thin:@localhost:1521:orcl";
+		String user = "javadb";
+		String password = "12345";
+	}
+	
 }
