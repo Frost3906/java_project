089334b1f1722f55ac786237bde9deb0865df0f2
YuseongPark <pus1125@gmail.com>
2020-04-24 12:41:26 +0900

    게시판 창 가운데에 뜨게 하기

--- src/board/Board.java
+++ src/board/Board.java
@@ -71,6 +71,7 @@ public class Board extends JFrame implements ActionListener{
 		setResizable(false);
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(100, 100, 900, 450);
+		setLocationRelativeTo(null);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		contentPane.setLayout(new BorderLayout(0, 0));
