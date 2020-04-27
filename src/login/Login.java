000ada04804be726a246f99d1c185ff6814a8df4
soddi <cvb7412@naver.com>
2020-04-27 19:14:25 +0900

    We can do it

--- src/login/Login.java
+++ src/login/Login.java
@@ -37,6 +37,7 @@ public class Login extends JFrame implements ActionListener {
 	private JTextField txt_id = new JTextField();	
 	private JLabel lb_pw = new JLabel("Password");	
 
+	
 	private JPasswordField txt_pw = new JPasswordField();	
 
 
@@ -110,7 +111,8 @@ public class Login extends JFrame implements ActionListener {
 
 		setVisible(true);
 		btn_login.addActionListener(this);
-		btn_reg.addActionListener(this);			
+		btn_reg.addActionListener(this);	
+		
 	}
 	
 
